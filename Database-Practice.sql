829e4159bd4b354009d48d9105b8add61bac1a92
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-22 10:06:07 -0700

    Update Database-Practice.sql

--- Database-Practice.sql
+++ Database-Practice.sql
@@ -7,3 +7,16 @@ SET NOCOUNT ON; -- Report only errors
 -- --------------------------------------------------------------------------------
 --						 
 -- --------------------------------------------------------------------------------
+
+-- Drop Table Statements
+
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('Torders')           IS NOT NULL    DROP TABLE Torders
+IF OBJECT_ID ('TStatuses')         IS NOT NULL    DROP TABLE TStatuses
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
+IF OBJECT_ID ('TorderProducts')    IS NOT NULL    DROP TABLE TorderProducts
