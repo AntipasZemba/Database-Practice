e5ecc073948b9467af52bd14413b4e249247fae9
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-24 10:32:06 -0700

    Update Database-Practice.sql

--- Database-Practice.sql
+++ Database-Practice.sql
@@ -43,88 +43,88 @@ CREATE TABLE TCustomers
 
 CREATE TABLE TCities
 (
-  intCityID			INTEGER		NOT NULL
- ,strCity			VARCHAR(255)	NOT NULL
+  intCityID		INTEGER		NOT NULL
+ ,strCity		VARCHAR(255)	NOT NULL
  ,CONSTRAINT TCities_PK PRIMARY KEY ( intCityID )
 )
 
 CREATE TABLE TStates
 (
- intStateID			INTEGER		NOT NULL
- ,strState			VARCHAR(255)	NOT NULL
+ intStateID		INTEGER		NOT NULL
+ ,strState		VARCHAR(255)	NOT NULL
  ,CONSTRAINT TStates_PK PRIMARY KEY ( intStateID )
 )
 
 CREATE TABLE TRaces
 (
- intRaceID			INTEGER		NOT NULL
-,strRace			VARCHAR(255)	NOT NULL
+ intRaceID		INTEGER		NOT NULL
+,strRace		VARCHAR(255)	NOT NULL
 ,CONSTRAINT TRaces_PK PRIMARY KEY ( intRaceID )
 )
 
 CREATE TABLE TGenders
 (
- intGenderID		INTEGER			NOT NULL
-,strGender		VARCHAR(255)		NOT NULL
+ intGenderID		INTEGER		NOT NULL
+,strGender		VARCHAR(255)	NOT NULL
 ,CONSTRAINT TGenders_PK PRIMARY KEY ( intGenderID )
 )
 
 CREATE TABLE TOrders
 (
- intOrderID			INTEGER			NOT NULL
-,intCustomerID			INTEGER			NOT NULL
-,strOrderNumber			VARCHAR(255)		NOT NULL
-,intStatusID			INTEGER			NOT NULL
-,dtmOrderDate			DATETIME		NOT NULL
+ intOrderID		INTEGER		NOT NULL
+,intCustomerID		INTEGER		NOT NULL
+,strOrderNumber		VARCHAR(255)	NOT NULL
+,intStatusID		INTEGER		NOT NULL
+,dtmOrderDate		DATETIME	NOT NULL
 ,CONSTRAINT TOrders_PK PRIMARY KEY ( intOrderID )
 )
 
 CREATE TABLE TStatuses
 (
- intStatusID			INTEGER			NOT NULL
-,strStatus			VARCHAR(255)		NOT NULL
+ intStatusID		INTEGER		NOT NULL
+,strStatus		VARCHAR(255)	NOT NULL
 ,CONSTRAINT TStatuses_PK PRIMARY KEY ( intStatusID )
 )
 
 CREATE TABLE TProducts
 (
- intProductID			INTEGER			NOT NULL
-,intVendorID			INTEGER			NOT NULL
-,strProductName			VARCHAR(255)		NOT NULL
-,monCostofProduct		MONEY			NOT NULL
-,monRetailCost			MONEY			NOT NULL
-,intProductCategoryID		INTEGER			NOT NULL
-,intInventory			INTEGER			NOT NULL
+ intProductID		INTEGER		NOT NULL
+,intVendorID		INTEGER		NOT NULL
+,strProductName		VARCHAR(255)	NOT NULL
+,monCostofProduct	MONEY		NOT NULL
+,monRetailCost		MONEY		NOT NULL
+,intProductCategoryID	INTEGER		NOT NULL
+,intInventory		INTEGER		NOT NULL
 ,CONSTRAINT TProducts_PK PRIMARY KEY ( intProductID )
 )
 
 CREATE TABLE TProductCategories	
 (
- intProductCategoryID		INTEGER			NOT NULL
-,strProductCategory		VARCHAR(255)		NOT NULL
+ intProductCategoryID	INTEGER		NOT NULL
+,strProductCategory	VARCHAR(255)	NOT NULL
 ,CONSTRAINT TProductCategories_PK PRIMARY KEY ( intProductCategoryID )
 )
 
 CREATE TABLE TVendors
 (
- intVendorID			INTEGER			NOT NULL
-,strVendorName			VARCHAR(255)		NOT NULL
-,strAddress			VARCHAR(255)		NOT NULL
-,intCityID			INTEGER			NOT NULL
-,intStateID			INTEGER			NOT NULL
-,strZip				VARCHAR(255)		NOT NULL
-,strContactFirstName		VARCHAR(255)		NOT NULL
-,strContactLastName		VARCHAR(255)		NOT NULL
-,strContactPhone		VARCHAR(255)		NOT NULL
-,strContactEmail		VARCHAR(255)		NOT NULL
+ intVendorID		INTEGER		NOT NULL
+,strVendorName		VARCHAR(255)	NOT NULL
+,strAddress		VARCHAR(255)	NOT NULL
+,intCityID		INTEGER		NOT NULL
+,intStateID		INTEGER		NOT NULL
+,strZip			VARCHAR(255)	NOT NULL
+,strContactFirstName	VARCHAR(255)	NOT NULL
+,strContactLastName	VARCHAR(255)	NOT NULL
+,strContactPhone	VARCHAR(255)	NOT NULL
+,strContactEmail	VARCHAR(255)	NOT NULL
 ,CONSTRAINT TVendors_PK PRIMARY KEY ( intVendorID )
 )
 
 CREATE TABLE TOrderProducts
 (
- intOrderProductID		INTEGER			NOT NULL
-,intOrderID			INTEGER			NOT NULL
-,intProductID			INTEGER			NOT NULL
+ intOrderProductID	INTEGER		NOT NULL
+,intOrderID		INTEGER		NOT NULL
+,intProductID		INTEGER		NOT NULL
 ,CONSTRAINT TTOrderProducts_PK PRIMARY KEY ( intOrderProductID )
 )
 
@@ -199,70 +199,70 @@ FOREIGN KEY ( intCityID ) REFERENCES TCities (intCityID ) ON DELETE CASCADE
 --	Step #3 : Add Data - INSERTS
 -- --------------------------------------------------------------------------------
 INSERT INTO TStates( intStateID, strState)
-VALUES			(1, 'Ohio')
-				   ,(2, 'Kentucky')
-				   ,(3, 'Indiana')
+VALUES	(1, 'Ohio')
+	,(2, 'Kentucky')
+	 ,(3, 'Indiana')
 
 INSERT INTO TCities( intCityID, strCity)
-VALUES			(1, 'Cincinnati')
-				   ,(2, 'Florence')
-				   ,(3, 'Norwood')
-				   ,(4, 'Milford')
-				   ,(5, 'West Chester')
+VALUES	(1, 'Cincinnati')
+	,(2, 'Florence')
+	,(3, 'Norwood')
+	,(4, 'Milford')
+	 ,(5, 'West Chester')
 
 INSERT INTO TRaces( intRaceID, strRace)
-VALUES			(1, 'Hispanic')
-				   ,(2, 'African American')
-				   ,(3, 'Cuacasion')
-				   ,(4, 'Asian')
+VALUES	(1, 'Hispanic')
+	,(2, 'African American')
+	,(3, 'Cuacasion')
+	 ,(4, 'Asian')
 
 INSERT INTO TGenders( intGenderID, strGender)
-VALUES			(1, 'Male')
-				   ,(2, 'Female')
-				   ,(3, 'Other')
+VALUES	(1, 'Male')
+	,(2, 'Female')
+	 ,(3, 'Other')
 
 INSERT INTO TCustomers (intCustomerID, strFirstName, strLastName, strAddress, intCityID, intStateID, strZip, dtmDateOfBirth, intRaceID, intGenderID)
-VALUES			(1, 'James', 'Jones', '321 Elm St.', 1, 1, '45201', '1/1/1997', 1, 1)
-					 ,(2, 'Sally', 'Smith', '987 Main St.', 3, 1, '45218', '12/1/1999', 2, 2)
-					 ,(3, 'Jose', 'Hernandez', '1569 Windisch Rd.', 5, 1, '45069', '9/23/1998', 1, 1)
-					 ,(4, 'Lan', 'Kim', '44561 Oak Ave.', 4, 1, '45246', '6/11/1999', 4, 1)
-					 ,(5, 'Bob', 'Nields', '44561 Oak Ave.', 4, 1, '45246', '6/11/1999', 4, 1)
+VALUES	(1, 'James', 'Jones', '321 Elm St.', 1, 1, '45201', '1/1/1997', 1, 1)
+	,(2, 'Sally', 'Smith', '987 Main St.', 3, 1, '45218', '12/1/1999', 2, 2)
+	,(3, 'Jose', 'Hernandez', '1569 Windisch Rd.', 5, 1, '45069', '9/23/1998', 1, 1)
+	,(4, 'Lan', 'Kim', '44561 Oak Ave.', 4, 1, '45246', '6/11/1999', 4, 1)
+	,(5, 'Bob', 'Nields', '44561 Oak Ave.', 4, 1, '45246', '6/11/1999', 4, 1)
 
 INSERT INTO TStatuses( intStatusID, strStatus)
-VALUES			(1, 'Ordered')
-				   ,(2, 'Shipped')
-				   ,(3, 'Delivered')
+VALUES	(1, 'Ordered')
+	,(2, 'Shipped')
+	,(3, 'Delivered')
 
 
 INSERT INTO TOrders ( intOrderID, intCustomerID, strOrderNumber, intStatusID, dtmOrderDate)
-VALUES		 ( 1, 1, '10101010', 2, '8/28/2017')
-					,( 2, 1, '20202020', 1, '8/28/2007')
-					,( 3, 2, '30303030', 3, '6/28/2017')
+VALUES	( 1, 1, '10101010', 2, '8/28/2017')
+	,( 2, 1, '20202020', 1, '8/28/2007')
+	,( 3, 2, '30303030', 3, '6/28/2017')
 					,( 4, 4, '40404040', 3, '5/28/2007')
 
 INSERT INTO TVendors ( intVendorID, strVendorName, strAddress, intCityID, intStateID, strZip, strContactFirstName, strContactLastName, strContactPhone, strContactEmail)
-VALUES				 (1, 'TreesRUs', '321 Elm St.', 1, 1, '45201', 'Iwana', 'Cleantooth', '555-555-5555', 'Icleantooth@treesrus.com')
-						  ,(2, 'ShirtsRUs', '987 Main St.', 3, 1, '45218', 'Eilene', 'Totheright' , '666-666-6666', 'etotheright@shirtsrus.com')
-						  ,(3, 'ToysRUs', '1569 Windisch Rd.', 5, 1, '45069', 'Mike', 'Metosing', '888-888-8888', 'mmetosing@toysrus.com')					  
+VALUES	(1, 'TreesRUs', '321 Elm St.', 1, 1, '45201', 'Iwana', 'Cleantooth', '555-555-5555', 'Icleantooth@treesrus.com')
+	,(2, 'ShirtsRUs', '987 Main St.', 3, 1, '45218', 'Eilene', 'Totheright' , '666-666-6666', 'etotheright@shirtsrus.com')
+	,(3, 'ToysRUs', '1569 Windisch Rd.', 5, 1, '45069', 'Mike', 'Metosing', '888-888-8888', 'mmetosing@toysrus.com')					  
 
 INSERT INTO TProductCategories( intProductCategoryID, strProductCategory)
-VALUES			(1, 'Every Day')
-				   ,(2, 'Apparel')
-				   ,(3, 'Electronics')
+VALUES	(1, 'Every Day')
+	,(2, 'Apparel')
+	,(3, 'Electronics')
 
 
 INSERT INTO TProducts( intProductID, intVendorID, strProductName, monCostofProduct, monRetailCost, intProductCategoryID, intInventory)
-VALUES			   (1, 1,'Toothpicks', .10, .40, 1, 100000)
-						  ,(2, 2,'T-Shirts', 5.10, 15.40, 2, 2000)
-						  ,(3, 3,'uPlay', 44.10, 85.40, 3, 3)
+VALUES	(1, 1,'Toothpicks', .10, .40, 1, 100000)
+	,(2, 2,'T-Shirts', 5.10, 15.40, 2, 2000)
+	 ,(3, 3,'uPlay', 44.10, 85.40, 3, 3)
 
 INSERT INTO TOrderProducts ( intOrderProductID, intOrderID, intProductID)
-VALUES		 ( 1, 1, 1 )
-					,( 2, 1, 2 )
-					,( 3, 2, 3 )
-					,( 4, 3, 2 )
-					,( 5, 3, 3 )
-					,( 6, 4, 3 )
+VALUES	( 1, 1, 1 )
+	,( 2, 1, 2 )
+	,( 3, 2, 3 )
+	,( 4, 3, 2 )
+	,( 5, 3, 3 )
+	,( 6, 4, 3 )
 
 -- --------------------------------------------------------------------------------
 --	Step #4 : Explicit Joins
@@ -274,7 +274,7 @@ Select	 TC.intCustomerID
 		,TC.strFirstName
 		,TC.strLastName
 		,TOR.intOrderID
-	    ,TOR.dtmOrderDate
+	    	,TOR.dtmOrderDate
 		,TP.intProductID
 		,TP.strProductName
 From TCustomers AS TC 
@@ -338,7 +338,7 @@ Select	 TC.intCustomerID
 		,TOR.intOrderID
 		,TOR.dtmOrderDate
 		,TP.intProductID
-	    ,TP.strProductName
+	    	,TP.strProductName
 From TCustomers AS TC 
 	JOIN TOrders AS TOR
 		ON TC.intCustomerID = TOR.intCustomerID
