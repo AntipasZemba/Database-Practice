857abb93dd1f4fcb79a8f531dd0b69deb8798db4
Antipas Zemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-24 09:38:41 -0700

    Update Database-Practice.sql

--- Database-Practice.sql
+++ Database-Practice.sql
@@ -43,87 +43,154 @@ CREATE TABLE TCustomers
 
 CREATE TABLE TCities
 (
-	 intCityID			INTEGER			NOT NULL
-	,strCity			VARCHAR(255)	NOT NULL
-	,CONSTRAINT TCities_PK PRIMARY KEY ( intCityID )
+  intCityID			INTEGER		NOT NULL
+ ,strCity			VARCHAR(255)	NOT NULL
+ ,CONSTRAINT TCities_PK PRIMARY KEY ( intCityID )
 )
 
 CREATE TABLE TStates
 (
-	 intStateID			INTEGER			NOT NULL
-	,strState			VARCHAR(255)	NOT NULL
-	,CONSTRAINT TStates_PK PRIMARY KEY ( intStateID )
+ intStateID			INTEGER		NOT NULL
+ ,strState			VARCHAR(255)	NOT NULL
+ ,CONSTRAINT TStates_PK PRIMARY KEY ( intStateID )
 )
 
 CREATE TABLE TRaces
 (
-	 intRaceID			INTEGER			NOT NULL
-	,strRace			VARCHAR(255)	NOT NULL
-	,CONSTRAINT TRaces_PK PRIMARY KEY ( intRaceID )
+ intRaceID			INTEGER		NOT NULL
+,strRace			VARCHAR(255)	NOT NULL
+,CONSTRAINT TRaces_PK PRIMARY KEY ( intRaceID )
 )
 
 CREATE TABLE TGenders
 (
-	 intGenderID		INTEGER			NOT NULL
-	,strGender			VARCHAR(255)	NOT NULL
-	,CONSTRAINT TGenders_PK PRIMARY KEY ( intGenderID )
+ intGenderID		INTEGER			NOT NULL
+,strGender		VARCHAR(255)		NOT NULL
+,CONSTRAINT TGenders_PK PRIMARY KEY ( intGenderID )
 )
 
 CREATE TABLE TOrders
 (
-	 intOrderID				INTEGER			NOT NULL
-	,intCustomerID			INTEGER			NOT NULL
-	,strOrderNumber			VARCHAR(255)	NOT NULL
-	,intStatusID			INTEGER			NOT NULL
-	,dtmOrderDate			DATETIME		NOT NULL
-	,CONSTRAINT TOrders_PK PRIMARY KEY ( intOrderID )
+ intOrderID			INTEGER			NOT NULL
+,intCustomerID			INTEGER			NOT NULL
+,strOrderNumber			VARCHAR(255)		NOT NULL
+,intStatusID			INTEGER			NOT NULL
+,dtmOrderDate			DATETIME		NOT NULL
+,CONSTRAINT TOrders_PK PRIMARY KEY ( intOrderID )
 )
 
 CREATE TABLE TStatuses
 (
-	 intStatusID			INTEGER			NOT NULL
-	,strStatus				VARCHAR(255)	NOT NULL
-	,CONSTRAINT TStatuses_PK PRIMARY KEY ( intStatusID )
+ intStatusID			INTEGER			NOT NULL
+,strStatus			VARCHAR(255)		NOT NULL
+,CONSTRAINT TStatuses_PK PRIMARY KEY ( intStatusID )
 )
 
 CREATE TABLE TProducts
 (
-	 intProductID			INTEGER			NOT NULL
-	,intVendorID			INTEGER			NOT NULL
-	,strProductName			VARCHAR(255)	NOT NULL
-	,monCostofProduct		MONEY			NOT NULL
-	,monRetailCost			MONEY			NOT NULL
-	,intProductCategoryID	INTEGER			NOT NULL
-	,intInventory			INTEGER			NOT NULL
-	,CONSTRAINT TProducts_PK PRIMARY KEY ( intProductID )
+ intProductID			INTEGER			NOT NULL
+,intVendorID			INTEGER			NOT NULL
+,strProductName			VARCHAR(255)		NOT NULL
+,monCostofProduct		MONEY			NOT NULL
+,monRetailCost			MONEY			NOT NULL
+,intProductCategoryID		INTEGER			NOT NULL
+,intInventory			INTEGER			NOT NULL
+,CONSTRAINT TProducts_PK PRIMARY KEY ( intProductID )
 )
 
 CREATE TABLE TProductCategories	
 (
-	 intProductCategoryID	INTEGER			NOT NULL
-	,strProductCategory		VARCHAR(255)	NOT NULL
-	,CONSTRAINT TProductCategories_PK PRIMARY KEY ( intProductCategoryID )
+ intProductCategoryID		INTEGER			NOT NULL
+,strProductCategory		VARCHAR(255)		NOT NULL
+,CONSTRAINT TProductCategories_PK PRIMARY KEY ( intProductCategoryID )
 )
 
 CREATE TABLE TVendors
 (
-	 intVendorID			INTEGER			NOT NULL
-	,strVendorName			VARCHAR(255)	NOT NULL
-	,strAddress				VARCHAR(255)	NOT NULL
-	,intCityID				INTEGER			NOT NULL
-	,intStateID				INTEGER			NOT NULL
-	,strZip					VARCHAR(255)	NOT NULL
-	,strContactFirstName	VARCHAR(255)	NOT NULL
-	,strContactLastName		VARCHAR(255)	NOT NULL
-	,strContactPhone		VARCHAR(255)	NOT NULL
-	,strContactEmail		VARCHAR(255)	NOT NULL
-	,CONSTRAINT TVendors_PK PRIMARY KEY ( intVendorID )
+ intVendorID			INTEGER			NOT NULL
+,strVendorName			VARCHAR(255)		NOT NULL
+,strAddress			VARCHAR(255)		NOT NULL
+,intCityID			INTEGER			NOT NULL
+,intStateID			INTEGER			NOT NULL
+,strZip				VARCHAR(255)		NOT NULL
+,strContactFirstName		VARCHAR(255)		NOT NULL
+,strContactLastName		VARCHAR(255)		NOT NULL
+,strContactPhone		VARCHAR(255)		NOT NULL
+,strContactEmail		VARCHAR(255)		NOT NULL
+,CONSTRAINT TVendors_PK PRIMARY KEY ( intVendorID )
 )
 
 CREATE TABLE TOrderProducts
 (
-	 intOrderProductID		INTEGER			NOT NULL
-	,intOrderID				INTEGER			NOT NULL
-	,intProductID			INTEGER			NOT NULL
-	,CONSTRAINT TTOrderProducts_PK PRIMARY KEY ( intOrderProductID )
+ intOrderProductID		INTEGER			NOT NULL
+,intOrderID			INTEGER			NOT NULL
+,intProductID			INTEGER			NOT NULL
+,CONSTRAINT TTOrderProducts_PK PRIMARY KEY ( intOrderProductID )
 )
+
+-- --------------------------------------------------------------------------------
+--	Step #2 : Establish Referential Integrity 
+-- --------------------------------------------------------------------------------
+--
+-- #	Child				Parent				Column
+-- -	-----				------				---------
+-- 1	TOrders				TCustomers			intCustomerID	
+-- 2	TProducts			TVendors			intVendorID
+-- 3	TOrderProducts			TOrders				intOrderID
+-- 4	TOrderProducts			TProducts			intProductID
+-- 5	TCustomers			TStates				intStateID
+-- 6	TCustomers			TCities				intCityID
+-- 7	TCustomers			TGenders			intGenderID
+-- 8	TCustomers			TRaces				intRaceID
+-- 9	TOrders				TStatuses			intStatusID
+-- 10	TProducts			TProductCategories		intProductCategoryID
+-- 11	TVendors			TStates				intStateID
+-- 12	TVendors			TCities				intCityID
+
+--1
+ALTER TABLE TOrders ADD CONSTRAINT TOrders_TCustomers_FK 
+FOREIGN KEY ( intCustomerID ) REFERENCES TCustomers ( intCustomerID )
+
+--2
+ALTER TABLE TProducts ADD CONSTRAINT TProducts_TVendors_FK 
+FOREIGN KEY ( intVendorID ) REFERENCES TVendors ( intVendorID )
+
+--3
+ALTER TABLE TOrderProducts ADD CONSTRAINT TOrderProducts_TOrders_FK 
+FOREIGN KEY ( intOrderID ) REFERENCES TOrders ( intOrderID )
+
+--4
+ALTER TABLE TOrderProducts ADD CONSTRAINT TOrderProducts_TProducts_FK 
+FOREIGN KEY ( intProductID ) REFERENCES TProducts ( intProductID )
+
+--5
+ALTER TABLE TCustomers ADD CONSTRAINT TCustomers_TStates_FK 
+FOREIGN KEY ( intStateID ) REFERENCES TStates (intStateID ) ON DELETE CASCADE
+
+--6
+ALTER TABLE TCustomers ADD CONSTRAINT TCustomers_TCities_FK 
+FOREIGN KEY ( intCityID ) REFERENCES TCities (intCityID ) ON DELETE CASCADE
+
+--7
+ALTER TABLE TCustomers ADD CONSTRAINT TCustomers_TRaces_FK 
+FOREIGN KEY ( intRaceID ) REFERENCES TRaces (intRaceID ) ON DELETE CASCADE
+
+--8
+ALTER TABLE TCustomers ADD CONSTRAINT TCustomers_TGenders_FK 
+FOREIGN KEY ( intGenderID ) REFERENCES TGenders (intGenderID ) ON DELETE CASCADE
+
+--9
+ALTER TABLE TOrders ADD CONSTRAINT TOrders_TStatuses_FK 
+FOREIGN KEY ( intStatusID ) REFERENCES TStatuses (intStatusID ) ON DELETE CASCADE
+
+--10
+ALTER TABLE TProducts ADD CONSTRAINT TProducts_TProductCategories_FK 
+FOREIGN KEY ( intProductCategoryID ) REFERENCES TProductCategories (intProductCategoryID ) ON DELETE CASCADE
+
+--11
+ALTER TABLE TVendors ADD CONSTRAINT TVendors_TStates_FK 
+FOREIGN KEY ( intStateID ) REFERENCES TStates (intStateID ) ON DELETE CASCADE
+
+--12
+ALTER TABLE TVendors ADD CONSTRAINT TVendors_TCities_FK 
+FOREIGN KEY ( intCityID ) REFERENCES TCities (intCityID ) ON DELETE CASCADE
